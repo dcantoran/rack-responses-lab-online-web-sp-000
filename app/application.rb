212058d50d8c0a6cff68876c924ae0596f8194e6
David Cantoran <davidcantoran36@gmail.com>
2020-02-25 20:01:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new 
     
-    if Time.now.hour >= 12
+    if Time.now.hour >= 16
       resp.write "Good Afternoon"
     else 
       resp.write "Good Morning"
